b02f02cb4c6419056fa4ba134d388e2879a6c057
Stinus00 <160496935+Stinus00@users.noreply.github.com>
2025-12-11 14:36:09 +0100

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
 	return [
-		{ title: "New React Router App" },
-		{ name: "description", content: "Welcome to React Router!" },
+		{ title: "SUPER COOL COBBLEMON PACK" },
+		{ name: "description", content: "It's so cool I swear - Me" },
 	];
 }
 
